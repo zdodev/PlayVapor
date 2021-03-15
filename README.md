34805f95795592239616e8e3ce089ab556f45f70
zdodev <iver0945@gmail.com>
2021-03-15 22:14:37 +0900

    📝: Routes 예제.

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# pt
+## PlayVapor
+
+Vapor 놀이 공간
+
+-   GET /
+
+>Hello!
+
+### 경로 지정
+
+-   GET /hello
+
+>   Hello, world!
+
+-   GET /hello/vapor
+
+>   Hello, Vapor!
+
+### Request 파라미터
+
+-   GET /hello/{name}
+
+    >   Hello, {name}!
+
+### 지원 메서드
+
+-   GET
+-   POST
+-   PATCH
+-   PU
+-   DELETE
+
+### Path Component
+
+-   Constant, (path): 정적 경로 요소
+-   Paramter, (:path): 동적 경로 요소
+-   Anything, (*): 단일 동적 경로 요소
+-   Catchall, (**): 다중 동적 경로 요소
+
+### Parameters
+
+-   Parameter path component(:path)를 사용할 경우 파라미터의 값이 `request.paramters` 에 저장된다.
+
+-   `get(_:as:)` 메서드를 사용하여 파라미터의 값을 형변환하여 사용할 수 있다.
+-   `Catchall` 을 사용하면 request.parameters에 경로가 저장된다. request.parameters.getCatchall() 메서드를 사용
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,12 +2,44 @@ import Fluent
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req in
-        return req.view.render("index", ["title": "Hello Vapor!"])
+//    app.get { req in
+//        return req.view.render("index", ["title": "Hello Vapor!"])
+//    }
+    app.get { request -> String in
+        return "Hello!"
     }
 
-    app.get("hello") { req -> String in
-        return "Hello, world!"
+    app.get("hello") { request -> String in
+        "Hello, world!"
+    }
+    
+    app.get("hello", "vapor") { request in
+        "Hello, Vapor!"
+    }
+    
+    app.get(["hello", "zdo"]) { request in
+        "Hello, zdo!"
+    }
+    
+    app.on(.GET, "hello", "ios") { request in
+        "Hello, iOS!"
+    }
+    
+    app.get("hello", ":name") { request -> String in
+        let name = request.parameters.get("name")!
+        return "Hello, \(name)!"
+    }
+    
+    app.get("number", ":x") { request -> String in
+        guard let int = request.parameters.get("x", as: Int.self) else {
+            throw Abort(.badRequest)
+        }
+        return "\(int) is a great number."
+    }
+    
+    app.get("hello", "**") { request -> String in
+        let name = request.parameters.getCatchall().joined(separator: " ")
+        return "Hello, \(name)!"
     }
 
     try app.register(collection: TodoController())
