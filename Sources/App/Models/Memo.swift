357ef6061a2e76f9f32ddb7771ab2dda8e5e8f0f
zdodev <iver0945@gmail.com>
2021-03-21 23:48:10 +0900

    ✨: Memo model 정의.

--- Sources/App/Models/Memo.swift
+++ Sources/App/Models/Memo.swift
@@ -9,11 +9,23 @@ final class Memo: Model, Content {
 
     @Field(key: "title")
     var title: String
+    
+    @Field(key: "description")
+    var description: String
+    
+    @Field(key: "date")
+    var date: String
+    
+    @Field(key: "status")
+    var status: String
 
     init() { }
 
-    init(id: UUID? = nil, title: String) {
+    init(id: UUID, title: String, description: String, date: String, status: String) {
         self.id = id
         self.title = title
+        self.description = description
+        self.date = date
+        self.status = status
     }
 }
