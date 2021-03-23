f735c5d5eee8266a490a59368e913614ac4a9ce4
zdodev <iver0945@gmail.com>
2021-03-23 21:53:59 +0900

    ✨: Content-type 검증 메서드 구현.

--- Sources/App/Controllers/MemoController.swift
+++ Sources/App/Controllers/MemoController.swift
@@ -3,6 +3,9 @@ import Vapor
 
 struct MemoController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
+        let test = routes.grouped("test")
+        test.post(use: tests)
+        
         let memos = routes.grouped("memos")
         memos.get(use: readAllMemo)
         
@@ -11,12 +14,18 @@ struct MemoController: RouteCollection {
         memo.patch(use: updateMemo)
         memo.delete(":index", use: deleteMemo)
     }
-
+    
+    private func tests(request: Request) throws -> String {
+        let memo = try request.content.decode(Memo.self)
+        return "ee"
+    }
+    
     func readAllMemo(request: Request) throws -> EventLoopFuture<[Memo]> {
         return Memo.query(on: request.db).all()
     }
-
+    
     func createMemo(request: Request) throws -> EventLoopFuture<Memo> {
+        try checkValidContentType(request.headers.contentType)
         let memo = try request.content.decode(Memo.self)
         return memo.save(on: request.db).map {
             memo
@@ -24,6 +33,7 @@ struct MemoController: RouteCollection {
     }
     
     func updateMemo(request: Request) throws -> EventLoopFuture<[Memo]> {
+        try checkValidContentType(request.headers.contentType)
         let memo = try request.content.decode(Memo.self)
         
         guard let index = memo.id else {
@@ -36,14 +46,14 @@ struct MemoController: RouteCollection {
                 .filter(\.$id == index)
                 .update()
         }
-
+        
         if let description = memo.description {
             _ = Memo.query(on: request.db)
                 .set(\.$description, to: description)
                 .filter(\.$id == index)
                 .update()
         }
-
+        
         if let date = memo.date {
             _ = Memo.query(on: request.db)
                 .set(\.$date, to: date)
@@ -60,7 +70,7 @@ struct MemoController: RouteCollection {
         
         return Memo.query(on: request.db).all()
     }
-
+    
     func deleteMemo(request: Request) throws -> EventLoopFuture<Memo> {
         let memo = Memo.find(request.parameters.get("index"), on: request.db)
             .unwrap(or: Abort(.notFound))
@@ -72,5 +82,16 @@ struct MemoController: RouteCollection {
             }
         
         return memo
+    }
+    
+    private func checkValidContentType(_ contentType: HTTPMediaType?) throws {
+        guard let contentType = contentType else {
+            throw Abort(.unsupportedMediaType)
+        }
+
+        let validContentType = HTTPMediaType(type: "application", subType: "json")
+        if contentType != validContentType {
+            throw Abort(.unsupportedMediaType)
+        }
     }
 }
