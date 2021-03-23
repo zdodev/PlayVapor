0df4b635b9b239f6ce5004d357666303a5ede90c
zdodev <iver0945@gmail.com>
2021-03-23 22:07:37 +0900

    ✨: 파라미터 유무 검증 기능 구현.

--- Sources/App/Models/Memo.swift
+++ Sources/App/Models/Memo.swift
@@ -1,41 +1,37 @@
 import Fluent
 import Vapor
 
-enum Type: String, Codable {
-    case todo
-    case doing
-    case done
-}
-
 final class Memo: Model, Content {
+    enum Status: String, Codable {
+        case todo
+        case doing
+        case done
+    }
+    
     static let schema = "memos"
     
     @ID(key: .id)
     var id: UUID?
 
     @Field(key: "title")
-    var title: String?
+    var title: String
     
     @Field(key: "description")
-    var description: String?
+    var description: String
     
     @Field(key: "date")
-    var date: String?
+    var date: String
     
     @Field(key: "status")
-    var status: String?
-    
-    @Enum(key: "type")
-    var type: Type
+    var status: Status
 
     init() { }
 
-    init(id: UUID, title: String, description: String, date: String, status: String, type: Type) {
+    init(id: UUID, title: String, description: String, date: String, status: Status) {
         self.id = UUID()
         self.title = title
         self.description = description
         self.date = date
         self.status = status
-        self.type = type
     }
 }
